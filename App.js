2cbaaf3b1a6350613f54f066efd33e8676d342d5
Praneeth <praneethgourla@gmail.com>
2024-07-27 14:04:32 +0530

    React create element

--- App.js
+++ App.js
@@ -9,6 +9,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div", {id:"main"}, [React.createElement("div",{id:"child1"},React.createElement("h1",{id:"heading1"},"N")),React.createElement("div",{id:"child2"},React.createElement("h1",{id:"heading2"},"heading2"))]);
+const heading = (<div id="parent">
+    <div id="child1">
+        <h1 id="heading1">Hello this is heading 1</h1>
+    </div>
+    <div id="child2">
+        <h1 id="heading2">Hello this is heading 2</h1>
+ 
+   </div>
+   </div>)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
