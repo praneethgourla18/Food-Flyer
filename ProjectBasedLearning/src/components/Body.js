ead586cad9e0dfdba7134b70816c55a701d90c89
Praneeth <praneethgourla@gmail.com>
2024-07-28 17:32:00 +0530

    Added filter by rating

--- ProjectBasedLearning/src/components/Body.js
+++ ProjectBasedLearning/src/components/Body.js
@@ -1,34 +1,30 @@
 import ResCard from "./ResCard";
 
-import restaurants from "../../utils/mockData";
+import initialRestaurants from "../../utils/mockData";
 
-const Body=()=>(
-    <div id="body">
-    
-   
-    <div id="allcontent">
-        {/* /* <ResCard resObj={restaurants[0]}/> */} 
+import { useState } from "react";
 
-        {/* //dynamically using map method to render array of restaurant objects
-        // key prop is required for react to track the items in the list
-        // in this case, we are using restaurant id as key
-        // this is a best practice to make sure react can identify and update the correct component when the data changes
-        // without the key prop, react might render a new component instead of updating the existing one
-        // which can lead to performance issues and bugs */}
-    
-       {
-        restaurants.map(i=>(
-            <ResCard key={i.info.id} resObj={i}/>
-        )
-       )
-       }
+const Body=()=>{
+    let [restaurants, setRestaurants] = useState(initialRestaurants);
 
-        {/* //statically */}
+    return (
+        <div id="body">
+            <div id="filter-btn">
+               
+                <button id="ratingfilter" onClick={()=>{
+                    const filtered=restaurants.filter(res=>res.info.avgRating>4);
+                    restaurants=setRestaurants(filtered);
+                }} >
+                    Filter By Rating
+                </button>
+            </div>
 
-         {/* <ResCard resName="GrameenKulf3" cuisine="Ice Cream" url="https://media-assets.swiggy.com/swigg15/ima4e/upload/fl_lossy,f_auto,q_auto,w_660/dkcduodu7fuqn5lqn8f2"/15 */}
-         </div>
-    </div>
-    
-)
-
+            <div id="allcontent">
+                {restaurants.map((i) => (
+                    <ResCard key={i.info.id} resObj={i} />
+                ))}
+            </div>
+        </div>
+    );
+}
 export default Body;
