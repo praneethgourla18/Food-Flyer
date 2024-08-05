4e5a73abbf828a2e15d086ec78d85098e4cda7ff
Praneeth Gourla <praneethgourla@gmail.com>
2024-08-05 16:53:45 +0530

    Removed mock data and fetched the api data

--- ProjectBasedLearning/src/components/Body.js
+++ ProjectBasedLearning/src/components/Body.js
@@ -1,18 +1,42 @@
 import ResCard from "./ResCard";
 
-import initialRestaurants from "../../utils/mockData";
 
-import { useState } from "react";
+
+import { useState ,useEffect  } from "react";
 
 const Body=()=>{
-    const [restaurants, setRestaurants] = useState(initialRestaurants);
+    const [restaurants, setRestaurants] = useState([]);
 
+    const fetchRestaurants = async () => {
+        const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4875418&lng=78.3953462&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const data = await response.json();
+        console.log(data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurants(data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      };
+    
+    useEffect(() => {
+        fetchRestaurants();
+      }, []);
+    
+
+      if(restaurants.length===0){
+       return(
+        <div id="loading">
+          <img 
+          src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" 
+          alt="Loading..."
+          width="480"
+          height="480"
+        />
+      </div>
+        );
+      }
     return (
         <div id="body">
             <div id="filter-btn">
                
                 <button id="ratingfilter" onClick={()=>{
-                    const filtered=restaurants.filter(res=>res.info.avgRating>4);
+                    const filtered=restaurants.filter(res=>res?.info?.avgRating>4);
                     setRestaurants(filtered);
                 }} >
                     Filter By Rating
@@ -21,7 +45,7 @@ const Body=()=>{
 
             <div id="allcontent">
                 {restaurants.map((i) => (
-                    <ResCard key={i.info.id} resObj={i} />
+                    <ResCard key={i?.info?.id} resObj={i} />
                 ))}
             </div>
         </div>
