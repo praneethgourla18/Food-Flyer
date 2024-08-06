89c8ba66c84260f8dbc68cc12dfa920edd293421
Praneeth Gourla <praneethgourla@gmail.com>
2024-08-06 11:14:04 +0530

    Added authentication button

--- Project-1/src/components/Header.js
+++ Project-1/src/components/Header.js
@@ -1,5 +1,10 @@
 import { LOGO_URL } from "../../utils/constant";
-const Header=()=>(
+import { useState } from "react";
+const Header=()=>{
+
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+    
+    return (
     <div id="header">
         <img className="logo" alt="logo" src={LOGO_URL}/>
 
@@ -11,9 +16,17 @@ const Header=()=>(
                 <li>About</li>
                 <li>Menu</li>
                 <li>Contact</li>
+                <button className="login"
+                onClick={() => {
+                         btnNameReact === "Login"
+                          ? setBtnNameReact("Logout")
+                         : setBtnNameReact("Login");
+                }} > {btnNameReact} </button>
             </ul>
+            
         </nav>
         
     </div>
 )
+}
 export default Header;
