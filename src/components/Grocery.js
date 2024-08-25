ab3ae7b20365fcf810bcdb04ca807f82f0cabdce
Praneeth gourla <praneethgourla@gmail.com>
2024-08-25 08:05:02 +0000

    added animation in grocery page

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -6,9 +6,9 @@ const Grocery = () =>{
           width="550px"
            height="400px" 
            src={waiting_png}
-           alt="login-rounded-right"
-           className="group-hover:invert group-hover:brightness-0 transition-all duration-200"
-           />
+          alt="waiting img"
+          className="transition-transform transform-gpu duration-500 ease-in-out hover:scale-x-90"
+         />
     </div>
   )
 }
