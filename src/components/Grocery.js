df4c84fee9afec0467343b44b28d44e2ec8276a0
Praneeth gourla <praneethgourla@gmail.com>
2024-08-25 05:54:55 +0000

    Styled Grocery page

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,5 +1,15 @@
+import waiting_png from "../utils/waiting.png"
 const Grocery = () =>{
-
-  return <h1>Coming SOON!</h1>
+  return(
+    <div className="mt-[80px] flex justify-center">
+       <img
+          width="550px"
+           height="400px" 
+           src={waiting_png}
+           alt="login-rounded-right"
+           className="group-hover:invert group-hover:brightness-0 transition-all duration-200"
+           />
+    </div>
+  )
 }
 export default Grocery;
