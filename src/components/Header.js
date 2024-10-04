86e669080cef3f810318bbbf9abc3ac44b579062
Praneeth gourla <praneethgourla@gmail.com>
2024-10-04 09:31:47 +0000

    Styled bag and profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,24 +20,16 @@ const Header = () => {
             <div className="flex justify-between items-center bg-[#FFF] shadow-md fixed top-0 right-0 left-0 z-50  md:h-[80px] px-5">
                 <img className="w-[130px] h-[80px] md:w-[190px] relative" alt="logo" src={logo} />
                 <div className="flex items-center md:gap-3 gap-2">
-                <div className="md:hidden">
-                    {/* <Link to="/Cart">Ca rt({cartItems.length})</Link> */}
-                    <Link to="/Cart" className="relative group">
-                      <img 
-                        className="w-8 h-8 group-hover:text-[#d4af37] transition-colors duration-300" 
-                        src="https://img.icons8.com/material-two-tone/33/shopping-bag--v1.png" 
-                        alt="shopping-bag--v1"
-                      />
-                      {/* Cart Item Count */}
-                      <span className={(cartItems.length > 9) 
-                        ? "absolute -top-2 -right-2 bg-[#d4af37] text-white rounded-full text-xs w-5 h-5 flex items-center justify-center font-bold group-hover:bg-[#fff] group-hover:text-[#d4af37] transition-all duration-300"
-                        : "absolute -top-1.5 -right-1.5 bg-[#d4af37] text-white rounded-full text-xs w-4 h-4 flex items-center justify-center font-bold group-hover:bg-[#fff] group-hover:text-[#d4af37] transition-all duration-300"
-                      }>
-                        {cartItems.length}
-                      </span>
-                    </Link>
+                    <div className="md:hidden active:underline">
+                        <Link to="/Cart" className="relative group">
+                            <img width="25" height="25" src="https://img.icons8.com/ios/25/shopping-bag--v1.png" alt="shopping-bag--v1" />
+                            {/* Cart items length with the gold theme */}
+                            <span className={`absolute -top-3 ${cartItems.length <= 9 ? '-right-3' : '-right-4'} bg-[#d4af37] text-white text-xs font-bold rounded-full px-1.5 py-0.5 border-2 border-white`}>
+                                {cartItems.length > 9 ? "9+" : cartItems.length}
+                            </span>
+                        </Link>
+                    </div>
 
-                </div>
 
                              
                 <nav id="navbar" className='hidden md:flex mr-3'>
@@ -84,7 +76,7 @@ const Header = () => {
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                     aria-label="Menu"
                 >
-                    <img width="33" height="33" src="https://img.icons8.com/plumpy/33/user-male-circle.png" alt="user-male-circle"/>
+                    <img width="33" height="33" src="https://img.icons8.com/pastel-glyph/33/gender-neutral-user.png" alt="gender-neutral-user"/>
                 </button>
                 </div>
             </div>
