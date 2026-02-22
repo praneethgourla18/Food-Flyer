46d26288c04e40884917826ca0e89ca473967af1
Praneeth gourla <praneethgourla@gmail.com>
2026-02-22 15:15:37 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className="flex justify-center p-4 items-center bg-[#FFF] mt-[30px] ">
-            <h1 className="font-medium cursive" >Crafted with ❤️ by Praneeth Gourla</h1>
+            <h1 className="font-medium cursive" ></h1>
         </div>
     )
 }
